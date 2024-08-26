35389057262f29b1f6aa2c5f60fc8076cfedfda0
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2024-08-26 12:59:16 +0200

    build: libreria minificar css y js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const {src, dest, watch} = require('gulp');
 const sass = require('gulp-sass') (require('sass')); //importamos del node_modules
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+//JS
+const terser = require('gulp-terser-js');
 
 
 function css(done){
 
     src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass()) 
+        .pipe(postcss( [autoprefixer(), cssnano() ] ))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('assets/css')); 
     
     done(); //callback avisa a gulp cuando llega al final la función
@@ -15,6 +24,9 @@ function css(done){
 function javascript(done){
 
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init()) 
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('assets/js'));
     done();
 }
